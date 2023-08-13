fb2b375fce0abeeeede3eac480af66c1e100815e
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-13 22:32:10 +0900

    [Add] headerに必要なリンクを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_parmitted_parameters. if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行されます。
   
    # protectedは呼び出された他のコントローラからも参照することができます。
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
    # ストロングパラメータの設定
   # configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことで
   # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています。
-  def configure_parmitted_parameters
-    devise_parameter_sanitizer.parmit(:sign_up, keys: [:name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get '/home/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   resources :books
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
