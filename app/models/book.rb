2e07d3526e2c38633be0a1660b1879643424d0dc
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-16 14:44:13 +0900

    [Fix] bookのshowページの投稿者画像の記述を修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,13 +11,5 @@ class Book < ApplicationRecord
      validates :body, presence: true
      validates :image, presence: true
     
-    # get_image メソッド =特定の処理を名前で呼び出すことができる
-    # unless = 画像が設定されない場合はimagesに格納されているno_image.jpgという画像をデフォルト画像としてActiveStorageに格納する
-    def get_image
-      unless image.attached?
-        file_path = Rails.root.join('app/assets/images/no_image.jpg')
-        image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      end
-      image
-    end
+    
 end
