6efc310d988ecf8781ab932da0749f04ef895a26
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-17 12:14:11 +0900

    [Add] userのupdate機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def update
+  def update 
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
