91ceadcac4f71e68c8e496f035984ba5b3e4f291
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-17 12:14:52 +0900

    [Add] userを更新する記述が正しく実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   
   def update 
     @user = User.find(params[:id])
-    @user.update(user_params)
+    @user.update(user_params) 
     redirect_to user_path(@user.id)
   end
   
